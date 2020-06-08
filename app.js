468b75b1fd2b884b2a2a47c7eca1e7dba2b0e0a5
Firdaus Helmy <46524543+firdaushelmy@users.noreply.github.com>
2020-06-08 22:24:22 +0800

    add itemList and push added tasklist to list.ejs

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const itemsList = [];
 app.get('/', function (req, res) {
-  const testOnline = 'page is online'
-  res.render('list', { testPage: testOnline })
+  const testOnline = 'page is working as intended'
+  res.render('list', { testPage: testOnline, taskList: itemsList })
+})
+
+app.post('/', function (req, res) {
+  let item = req.body.siham
+  itemsList.push(item)
+  res.redirect('/');
 })
 
 
